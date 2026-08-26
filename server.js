5010ef7d1b28cedc3942bb4ab613ec69479137a2
hanxelto-blm <bhanxel600@gmail.com>
2026-08-26 13:20:30 -0600

    guardar

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 3001;
 
 // Servir archivos estáticos desde /public
 app.use(express.static(path.join(__dirname, 'public')));
